b9a244b10eda3834a31dac3955732586481dd503
dangherve <dangherve@gmail.com>
2026-03-12 13:46:06 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/Lucaslhm/Flipper-IRDB.git
+git fetch upstream
+git rebase upstream/main
+git remote set-url origin git@github.com:dangherve/Flipper-IRDB.git
+git push --force --set-upstream origin main
